efd0feac04e155cf6e0cfd500c1e5ffdfaf1d053
akshat0907 <122808480+akshat0907@users.noreply.github.com>
2025-05-22 18:18:54 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,25 @@
+import os
+import streamlit as st
+st.title('Spam Detection')
+
+from langchain_openai import OpenAI
+os.environ['OPENAI_API_KEY'] = 'AIzaSyBl31kKtxz3e4mIpm9oTqfRi3gcth3I-1g'
+model = OpenAI(temperature = 0.6)
+
+user_input = st.text_area("Enter Your Message")
+
+
+if(st.button('Answer')):
+    prompt = f"""
+    You are an expert linguist, who is good at classifying the given message into Spam/NotSpam labels.
+    Help me classify Message into: Spam(Ye toh SPAM Hai!!!) and NotSpam(Ye SPAM nhi Hai).
+    
+    ```
+    {user_input}
+    ```
+    """
+    response = model.invoke(prompt)
+    st.success(response)
+
+
+
